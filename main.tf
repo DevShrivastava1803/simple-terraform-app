0d240440a1d96ccf60c5eab02e6d80154c33bcab
DevShrivastava1803 <devshrivastava1803@gmail.com>
2025-09-15 12:15:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
-
+terraform {
+  backend "s3"{ 
+    bucket = "s3backendbucketdev124535" 
+    dynamodb_table = "state-lock" 
+    key = "global/s3/terraform.tfstate" 
+    region = "us-east-1" 
+    encrypt = true 
+} 
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 3.74.2"
+        }
+    }
+    required_version = ">= 1.3.0"
+}
 
 module "ec2" {
   source = "./modules/ec2"
-}
-
+} 
+ 
 module "s3" {
   source = "./modules/s3"
 }
