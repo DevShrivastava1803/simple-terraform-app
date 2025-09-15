b1ee0b4714196e4bc9fc834fd5ce93e112578371
DevShrivastava1803 <devshrivastava1803@gmail.com>
2025-09-15 12:15:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,27 +1,26 @@
-#create s3 
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "s3backendbucketdev124535"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+# #create s3 
+# resource "aws_s3_bucket" "mybucket" {
+#   bucket = "s3backendbucketdev124535"
+#   versioning {
+#     enabled = true
+#   }
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
+# # create dynamodb table
+# resource "aws_dynamodb_table" "state_lock" {
+#   name         = "state-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-# create dynamodb table
-resource "aws_dynamodb_table" "state_lock" {
-  name         = "state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
